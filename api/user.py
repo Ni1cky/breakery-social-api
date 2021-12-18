776594ad1297d2d5b761c2ca1d971d065e8eaaa3
Chernenkiy <nikmbogdanov@gmail.com>
2021-12-18 23:42:27 +0300

    Handles fixed

--- api/user.py
+++ api/user.py
@@ -1,55 +1,32 @@
 from api import app
 from store.models.models import User
-from store.user import get_user_by_id, get_user_by_login, add_user, delete_user, change_users_name_by_id, \
-    change_users_name_by_login
+from store.user import get_user_by_id, add_user, delete_user, get_users, update_user
 
 
-@app.get("/get_user")
-def get_user(user_id: int = -1, login: str = "") -> User:
-    if user_id != -1:
-        user = get_user_by_id(user_id)
-        return user
-    if login:
-        user = get_user_by_login(login)
-        return user
-    print("Дурак?")
+@app.get("/users")
+def get_all_users():
+    users = get_users()
+    return users
 
 
-@app.post("/add_user/{user}")
-def add_new_user(user: User):
+@app.post("/users")
+def add_new_user(req_user):
+    user = User(**req_user.dict)
     add_user(user)
 
 
-@app.delete("/delete_user/{user}")
-def delete_the_user(user: User):
-    delete_user(user)
+@app.delete("/users/{user_id}")
+def delete_the_user(user_id: int):
+    delete_user(user_id)
 
 
-@app.post("/change_name")
-def change_users_name(user_id: int = -1, login: str = "", new_name: str = ""):
-    if not new_name:
-        print("Дурак?")
-        return
-
-    if user_id != -1:
-        change_users_name_by_id(user_id, new_name)
-        return
-    if login:
-        change_users_name_by_login(login, new_name)
-        return
-    print("Дурак?")
+@app.post("/users/{id}/edit")
+def change_users_name(req_user):
+    user = User(**req_user.dict)
+    update_user(user)
 
 
-@app.post("/change_surname")
-def change_users_surname(user_id: int = -1, login: str = "", new_surname: str = ""):
-    if not new_surname:
-        print("Дурак?")
-        return
-
-    if user_id != -1:
-        change_users_name_by_id(user_id, new_surname)
-        return
-    if login:
-        change_users_name_by_login(login, new_surname)
-        return
-    print("Дурак?")
+@app.get("/users/{id}")
+def get_user(user_id: int):
+    user = get_user_by_id(user_id)
+    return user
--- store/user.py
+++ store/user.py
@@ -17,46 +17,22 @@ def get_user_by_id(user_id: int) -> User:
         return user
 
 
-def get_user_by_login(login: str) -> User:
+def delete_user(user_id):
     with session_factory() as session:
         session: Session
-        user = session.query(User).filter_by(login=login).first()
-        return user
-
-
-def delete_user(user: User):
-    with session_factory() as session:
-        session: Session
+        user = session.query(User).filter_by(id=user_id).first()
         session.delete(user)
 
 
-def change_users_name_by_id(user_id: int, new_name: str):
+def get_users():
     with session_factory() as session:
         session: Session
-        user = session.query(User).filter_by(id=user_id).first()
-        user.name = new_name
-        session.commit()
+        users = session.query(User).all()
+        return users
 
 
-def change_users_name_by_login(login: str, new_name: str):
+def update_user(user: User):
     with session_factory() as session:
         session: Session
-        user = session.query(User).filter_by(login=login).first()
-        user.name = new_name
-        session.commit()
-
-
-def change_users_surname_by_id(user_id: int, new_surname: str):
-    with session_factory() as session:
-        session: Session
-        user = session.query(User).filter_by(id=user_id).first()
-        user.surname = new_surname
-        session.commit()
-
-
-def change_users_surname_by_login(login: str, new_surname: str):
-    with session_factory() as session:
-        session: Session
-        user = session.query(User).filter_by(login=login).first()
-        user.surname = new_surname
+        session.add(user)
         session.commit()
