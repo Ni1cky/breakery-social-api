8befb22e33403e059d2e98470fd9cfc1b0046f28
Chernenkiy <nikmbogdanov@gmail.com>
2021-12-19 14:14:32 +0300

    Routers added

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from api.message import message_router
+from api.user import user_router
 
 app = FastAPI()
+app.include_router(user_router)
+app.include_router(message_router)
