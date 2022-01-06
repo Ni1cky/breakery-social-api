81d6493ee7d9285482d417e2479383e5b18b02a2
vadlord <vadim.t0lstykh@yandex.ru>
2022-01-06 19:15:02 +0300

    all done

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from api.user import user_router
 from api.auth import auth_router
 
 app = FastAPI()
+app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(message_router)
-app.include_router(auth_router)
+
