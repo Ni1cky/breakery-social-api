969aa174b6490efd3d9866fd1ee3326209dbb922
Liza <e.avdeeva00@mail.ru>
2022-01-29 22:34:25 +0300

    add functions to create/update/get/delete post photos and to get photos from all user posts

--- api/photo.py
+++ api/photo.py
@@ -1,15 +1,10 @@
 from fastapi import APIRouter
-from store.models.models import Photo
-from store.photo import get_all_user_photos, update_profile_picture, get_profile_picture, set_default_profile_picture
+from store.photo import update_profile_picture, get_profile_picture, set_default_profile_picture, \
+    add_photo_to_user_post, update_photo_from_user_post, get_photo_from_user_post, get_user_posts_photo, \
+    delete_photo_from_user_post
 from store.models.schemes import PhotoBase
 
 photo_router = APIRouter()
-
-
-@photo_router.get("/photos/{user_id}")
-def get_user_photos(user_id: int):
-    photos = get_all_user_photos(user_id)
-    return photos
 
 
 @photo_router.get("/photos/{user_id}/profile")
@@ -26,3 +21,30 @@ def set_user_profile_picture(user_id: int, img: PhotoBase):
 @photo_router.post("/photos/default")
 def set_to_user_default_picture(login: str, img: PhotoBase):
     set_default_profile_picture(login, img.source)
+
+
+@photo_router.post("/photos/{user_id}/{post_id}")
+def add_photo_to_post(user_id: int, post_id: int, img: PhotoBase):
+    add_photo_to_user_post(user_id, post_id, img.source)
+
+
+@photo_router.put("/photos/{user_id}/{post_id}")
+def update_photo_from_post(user_id: int, post_id: int, img: PhotoBase):
+    update_photo_from_user_post(user_id, post_id, img.source)
+
+
+@photo_router.get("/photos/{user_id}/{post_id}")
+def get_photo_from_post(user_id: int, post_id: int):
+    photo = get_photo_from_user_post(user_id, post_id)
+    return photo
+
+
+@photo_router.delete("/photos/{user_id}/{post_id}")
+def delete_photo_from_post(user_id: int, post_id: int):
+    delete_photo_from_user_post(user_id, post_id)
+
+
+@photo_router.get("/photos/{user_id}")
+def get_all_user_posts_photo(user_id: int):
+    photos = get_user_posts_photo(user_id)
+    return photos
