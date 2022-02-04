54a4ec256a8b571c9dc672c42d69428cda1af9c2
Chernenkiy <nikmbogdanov@gmail.com>
2022-02-04 22:36:05 +0300

    Added router for getting dialogs of a user

--- api/dialog.py
+++ api/dialog.py
@@ -6,6 +6,12 @@ from store.models.schemes import DialogCreate
 dialog_router = APIRouter()
 
 
+@dialog_router.get("/dialogs/all/{user_id}")
+def get_all_dialogs_for_user(user_id: int):
+    dialogs = get_users_dialogs(user_id)
+    return dialogs
+
+
 @dialog_router.get("/dialogs/{dialog_id}")
 def get_dialog(dialog_id: int):
     dialog = get_dialog_by_id(dialog_id)
@@ -22,9 +28,3 @@ def create_dialog(req_dialog: DialogCreate):
 def get_dialog_between_two_users(user1_id: int, user2_id: int):
     dialog = get_dialog_by_users_ids(user1_id, user2_id)
     return dialog
-
-
-@dialog_router.get("/dialogs/all/{user_id}")
-def get_all_dialogs_for_user(user_id: int):
-    dialogs = get_users_dialogs(user_id)
-    return dialogs
