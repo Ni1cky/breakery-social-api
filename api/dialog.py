ce33ea05659aa1809e09e0ca91fffcc5cf3a1381
Chernenkiy <nikmbogdanov@gmail.com>
2022-02-04 20:38:45 +0300

    Nothing useful

--- api/dialog.py
+++ api/dialog.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-
-from store.dialog import get_dialog_by_id, add_dialog, get_dialog_by_users_ids
+from store.dialog import get_dialog_by_id, add_dialog, get_dialog_by_users_ids, get_users_dialogs
 from store.models.models import Dialog
 from store.models.schemes import DialogCreate
 
@@ -23,3 +22,9 @@ def create_dialog(req_dialog: DialogCreate):
 def get_dialog_between_two_users(user1_id: int, user2_id: int):
     dialog = get_dialog_by_users_ids(user1_id, user2_id)
     return dialog
+
+
+@dialog_router.get("/dialogs/all/{user_id}")
+def get_all_dialogs_for_user(user_id: int):
+    dialogs = get_users_dialogs(user_id)
+    return dialogs
--- store/dialog.py
+++ store/dialog.py
@@ -1,6 +1,5 @@
 from sqlalchemy import and_, or_
 from sqlalchemy.orm import Session
-
 from store import session_factory
 from store.models.models import Dialog
 
@@ -27,3 +26,12 @@ def get_dialog_by_users_ids(user1_id: int, user2_id: int):
                 and_(Dialog.user1_id == user2_id, Dialog.user2_id == user1_id))
         ).first()
         return dialog
+
+
+def get_users_dialogs(user_id: int):
+    with session_factory() as session:
+        session: Session
+        dialogs = session.query(Dialog).filter(
+            or_(Dialog.user1_id == user_id, Dialog.user2_id == user_id)
+        ).all()
+        return dialogs
